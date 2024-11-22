89c410d45b3253fba55fa6feef458ee79c4be8a6
PeterFujiyu <jiyu.fu@outlook.com>
2024-11-22 19:29:51 +0800

    Add "bug"

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
 int pell(int N) {
 	if (N == 1) {
@@ -24,14 +24,38 @@ int Fibonacci(int num) {
 	}
 }
 
+//int main() {
+//	int a;
+//	int num[10001] = {};
+//	cin >> a; 
+//	for (int i=0;i<a;i++) {
+//		cin >> num[i]; 
+//	}
+//	for (int i=0;i<a;i++) {
+//		cout << pell(num[i]) << endl;
+//	}
+//}
+
 int main() {
-	int a;
-	int num[10001] = {};
-	cin >> a; 
-	for (int i=0;i<a;i++) {
-		cin >> num[i]; 
-	}
-	for (int i=0;i<a;i++) {
-		cout << pell(num[i]) << endl;
-	}
+    int x, y, z;
+
+    cout << "输入 x（成虫开始产卵的月数）、y（每对成虫每次产的卵数）、z（目标月数）: ";
+    cin >> x >> y >> z;
+
+    int adults = 1;
+    int eggsLastMonth = 0, eggsTwoMonthsAgo = 0;
+
+    for (int month = 1; month <= z; ++month) {
+        int newAdults = eggsTwoMonthsAgo;
+        eggsTwoMonthsAgo = eggsLastMonth;
+        if (month >= x) {
+            eggsLastMonth = adults * y;
+        } else {
+            eggsLastMonth = 0; 
+        }
+        adults += newAdults;
+    }
+    cout << "第 " << z << " 个月后成虫的数量是: " << adults << endl;
+    return 0;
 }
+
