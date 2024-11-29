52ffbfa4f77c9f676bb085a5855b94c005916d7c
PeterFujiyu <jiyu.fu@outlook.com>
2024-11-29 19:01:31 +0800

    Add factorial code.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,28 @@
 #include <cstdio>
 using namespace std;
 
+//int main() {
+//	FILE *fin, *fout;
+//	fin=fopen("simple.in","rb");
+//	fout=fopen("simple.out","wb");
+//	
+//	int a,b,s;
+//	fscanf(fin,"%d %d",&a,&b);
+//	s=a*b;
+//	fprintf(fout,"%d\n",stdout);
+//	
+//	fclose(stdin);
+//	fclose(stdout);
+//	return 0; 
+//}
+
+int factorial(int n) {
+	if (n==1) return 1;
+	return n*factorial(n-1);
+}
+
 int main() {
-	FILE *fin, *fout;
-	fin=fopen("simple.in","rb");
-	fout=fopen("simple.out","wb");
-	
-	int a,b,s;
-	fscanf(fin,"%d %d",&a,&b);
-	s=a*b;
-	fprintf(fout,"%d\n",stdout);
-	
-	fclose(stdin);
-	fclose(stdout);
-	return 0; 
+	int i=0;
+	cin >> i;
+	cout << factorial(i) << endl;
 }
